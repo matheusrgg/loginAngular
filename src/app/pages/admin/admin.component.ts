0542cacfa7055d27e193757c09e322088cc9ebbc
matheusrgg <matheusrgg@gmail.com>
2023-09-30 12:16:53 -0300

    estudando rxjs

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,17 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoginService } from '../login/service/login.service';
+import { FornecedorService } from 'src/app/services/fornecedor.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.sass']
 })
-export class AdminComponent {
+export class AdminComponent implements OnInit {
+
+  fornecedores!: Observable<any[]>;
+
   constructor(
     public router: Router,
-    private loginService: LoginService
+    private loginService: LoginService,
+    private fornecedorSerivce: FornecedorService
   ) { }
+
+  ngOnInit(): void {
+    //pq esse da erro?retorna undefined e  o outro usado no projeto da Cathu não
+    //falar dos environments que nessa versão saiu, onde eu vejo e fico sabendo das mudanças das versões
+    //
+    // this.fornecedorSerivce.getFornecedor().subscribe(datas => (
+    //   console.log("cadeeeeeeeeeeee", this.fornecedores = datas)));
+    // console.log("eaeraer", this.fornecedores);
+
+    //opcao 2 ok
+    // this.fornecedorSerivce.getFornecedor().subscribe(datas => {
+    //   console.log("cadeeeeeeeeeeee", this.fornecedores = datas);
+    //   console.log("eaeraer", this.fornecedores);
+    // });
+
+    //vou tentar essa
+    this.fornecedores = this.fornecedorSerivce.getFornecedor()
+  }
 
   onLogout() {
     this.loginService.removeToken();
---
+++ src/app/services/fornecedor.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import jwt_decode from "jwt-decode";
+import { Observable } from "rxjs";
+@Injectable({
+    providedIn: 'root'
+})
+export class FornecedorService {
+
+    private encodedToken: string | null = null;
+    private readonly tokenKey = 'authToken';
+
+
+    constructor(
+        public http: HttpClient,
+    ) {
+
+    }
+
+
+
+    public createFornecedor({ email, senha }: any) {
+        const url = 'http://localhost:4000/fornecedor/create';
+        return this.http.post<any>(url, {
+            email: email,
+            senha: senha
+        })
+    }
+
+    public getFornecedor(): Observable<any[]> {
+        // OPCAO 1
+        // const url = 'http://localhost:4000/fornecedor/list';
+        // const cont = this.http.get<any>(url)
+        // console.log("teste", cont);
+        // return cont
+
+        //OPCAO 2
+        {
+            const getTodos: Observable<any[]> =
+                this.http.get<any[]>('http://localhost:4000/fornecedor/list');
+
+            return getTodos;
+        }
+
+    }
+
+
+
+
+
+
+}
